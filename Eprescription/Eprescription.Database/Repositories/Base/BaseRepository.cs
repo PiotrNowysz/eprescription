a0be2a977c79ea51492d4cf76076dbff678fe563
PiotrNowysz <pnowysz@gmail.com>
2021-02-14 11:25:25 +0100

    added abstract class BaseRepository with methods: GetAll, SaveChanges

---
+++ Eprescription/Eprescription.Database/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Eprescription.Database
+{
+    abstract class BaseRepository<Entity> where Entity : class
+    {
+        protected EprescriptionDbContext _dbContext;
+        protected abstract DbSet<Entity> DbSet { get; set; }
+        public BaseRepository(EprescriptionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public List<Entity> GetAll()
+        {
+            var entitiesList = new List<Entity>();
+            foreach (var entity in DbSet) entitiesList.Add(entity);
+            return entitiesList;
+        }
+        public void SaveChanges()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
+}
